486ae35eafa1128256be2d5a3224712f318fd7cc
ravvi-kumar <ravvi.kumar8989@gmail.com>
2024-08-16 08:15:55 +0530

    resume responsive

--- src/components/resume/index.tsx
+++ src/components/resume/index.tsx
@@ -1,18 +1,37 @@
 export default function Resume() {
   return (
     <div className="bg-white p-8">
-      <div className="grid grid-cols-2 gap-10">
-        <div>
-          <Heading />
-          <Education />
-          <Training />
-          <Experience />
-          <SideProjects />
-          <SoftSkills />
-          <Extra />
-          <Skills />
-          <Languages />
+      <div className="shadow-lg border-2 border-gray-200 rounded-lg p-10">
+        <div className="flex flex-col gap-4 lg:flex-row flex-wrap">
+          <div className="md:mr-4 flex-1">
+            <Heading />
+          </div>
+          <div className="lg:basis-[400px]">
+            <Info />
+          </div>
         </div>
+        <div className="flex flex-col gap-4 lg:flex-row flex-wrap">
+          <div className="md:mr-4 flex-1 py-4">
+            <Education />
+            <Training />
+          </div>
+          <div className="py-4 w-[300px] lg:w-auto lg:basis-[400px]">
+            <Skills />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 lg:flex-row flex-wrap">
+          <div className="md:mr-4 flex-1 py-4">
+            <Experience />
+          </div>
+          <div className="py-4 lg:basis-[400px]">
+            <Languages />
+          </div>
+        </div>
+        <div className="py-4">
+          <SideProjects />
+        </div>
+        <SoftSkills />
+        <Extra />
       </div>
     </div>
   );
@@ -48,6 +67,28 @@ function Heading() {
         additional responsibilities to meet team goals. Passionate about
         learning and committed to continual improvement.
       </p>
+    </div>
+  );
+}
+function Info() {
+  return (
+    <div>
+      <div>
+        <h3 className="font-bold">Address</h3>
+        <p>Mohali, Punjab, 140301</p>
+      </div>
+      <div>
+        <h3 className="font-bold">Phone</h3>
+        <p>+91 6283679170</p>
+      </div>
+      <div>
+        <h3 className="font-bold">Email</h3>
+        <p>ravi.kumar8989@gmail.com</p>
+      </div>
+      <div>
+        <h3 className="font-bold">Site</h3>
+        <p>https://ravi.vercel.app</p>
+      </div>
     </div>
   );
 }
@@ -112,6 +153,10 @@ function Experience() {
           Built a React Native application from scratch with biometric Auth, and
           Geo-fencing functionality to track location movement.
         </li>
+        <li>
+          Built a google slides chat bot with Cloudflare D1 database, Hono for
+          api and OpenAi, where users can chat with their google slides.
+        </li>
       </ul>
       <p className="mb-4">Aug 2022 - June 2023</p>
     </div>
@@ -167,8 +212,10 @@ function Extra() {
     <div>
       <h2 className="text-2xl font-bold mb-2">EXTRA</h2>
       <p>
-        React-query, zustand, redux-toolkit, svelte, Mongodb, Socket-io, Nestjs,
-        TypeOrm, Prisma, Expo, Express, Github, Gitlabs, Postman, Firebase
+        React-query , zustand, redux-toolkit, svelte, drizzle orm, Astrojs,
+        cloudflare workers, cloudflare pages, Honojs, Docker, Mongodb,
+        Socket-io, Nestjs, TypeOrm, Prisma, Expo, Express, Github, Gitlabs,
+        Postman, Firebase
       </p>
     </div>
   );
